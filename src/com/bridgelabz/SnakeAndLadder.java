1d6e445b8616bdbb4d09c5f1abf07baa3c3d2252
sriram036 <sriramravichandran2608@gmail.com>
2023-07-20 23:51:28 +0530

    Added: Die Roll

--- src/com/bridgelabz/SnakeAndLadder.java
+++ src/com/bridgelabz/SnakeAndLadder.java
@@ -1,8 +1,12 @@
 package com.bridgelabz;
 
+import java.util.Random;
+
 public class SnakeAndLadder {
     static final int STARTPOSITION = 0;
     public static void main(String[] args) {
         System.out.println("The player position is " + STARTPOSITION);
+        int dieValue = (int)(Math.random()*6-1+1)+1;
+        System.out.println("The die value is " + dieValue);
     }
 }
