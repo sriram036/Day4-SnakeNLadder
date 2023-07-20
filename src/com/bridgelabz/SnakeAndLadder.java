e1b251b1e31d31fd7d1493c543402c470dfd09a1
sriram036 <sriramravichandran2608@gmail.com>
2023-07-21 00:14:59 +0530

    Added: Play Till Win

--- src/com/bridgelabz/SnakeAndLadder.java
+++ src/com/bridgelabz/SnakeAndLadder.java
@@ -4,28 +4,33 @@ import java.util.Random;
 
 public class SnakeAndLadder {
     static final int STARTPOSITION = 0;
+    static final int FINALPOSITION = 100;
     public static void main(String[] args) {
         int playerPosition = 0;
         System.out.println("The player start position is " + STARTPOSITION);
-        int dieValue = (int)(Math.random()*6-1+1)+1;
-        System.out.println("The die value is " + dieValue);
-        playerPosition += dieValue;
-        System.out.println("The player position is " + playerPosition);
-        int option = (int)(Math.random()*3-1+1)+1;
-        switch (option) {
-            case 1: {
-                System.out.println("The player in the same position");
+        while (playerPosition <= FINALPOSITION) {
+            int dieValue = (int)(Math.random()*6-1+1)+1;
+            System.out.println("The die value is " + dieValue);
+            playerPosition += dieValue;
+            System.out.println("The player position is " + playerPosition);
+            int option = (int)(Math.random()*3-1+1)+1;
+            switch (option) {
+                case 1: {
+                    System.out.println("The player in the same position");
 
-            }break;
-            case 2: {
-                System.out.println("The player got the Ladder");
-                playerPosition += dieValue;
-            }break;
-            case 3: {
-                System.out.println("The player got the Snake");
-                playerPosition -= dieValue;
-            }break;
+                }break;
+                case 2: {
+                    System.out.println("The player got the Ladder");
+                    playerPosition += dieValue;
+                }break;
+                case 3: {
+                    System.out.println("The player got the Snake");
+                    playerPosition -= dieValue;
+                }break;
+            }
+            System.out.println("The player position is " + playerPosition);
         }
-        System.out.println("The player position is " + playerPosition);
+        System.out.println("The player wins.");
+        System.out.println("The player final position is " + playerPosition);
     }
 }
